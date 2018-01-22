31faae9c6d6a3b9d0d839d87d9c9b93dafd2ec1f
churkinv <churkinv@gmail.com>
2018-01-22 23:49:38 +0200

    Sorting algorithms project added, BubleSort initial

---
+++ Sorting/ISorter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public interface ISorter<T> : IPerformanceTracker
+       where T : IComparable<T>
+    {
+        void Sort(T[] items);
+    }
+}
